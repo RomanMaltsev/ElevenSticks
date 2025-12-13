4f772080f0b232266a3d47fe97dd927062c1a962
Roman Maltsev <roman.maltsev.realgar@proton.me>
2025-12-13 18:54:50 +0300

    feat: add server socket accepter

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -1,2 +1,36 @@
 #include "server.hpp"
-int server() { return 0; }
+#include "ret_codes.hpp"
+#include "rules.hpp"
+
+#include <iostream>
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+RetCodes server(Rules game_rules = Rules()) {
+    size_t turn = 0;
+    if (game_rules.players != 2) {
+        std::cerr << "Error: Only 2 players are supported in this version.\n";
+        return RetCodes::ERR_NOT_SUPPORTED;
+    }
+    int player[game_rules.players];
+    for (size_t i = 0; i < game_rules.players; ++i) {
+        player[i] = socket(AF_INET, SOCK_STREAM, 0);
+        sockaddr_in addr{};
+        addr.sin_family = AF_INET;
+        addr.sin_port = htons(8080);
+        addr.sin_addr.s_addr = INADDR_ANY;
+
+        bind(player[i], (sockaddr *)&addr, sizeof(addr));
+        std::cout << "Waiting for player " << (i + 1) << " to connect...\n";
+        listen(player[i], SOMAXCONN);
+        int client_fd = accept(player[i], nullptr, nullptr);
+        if (client_fd < 0) {
+            std::cerr << "Error: Failed to accept connection for player " << (i + 1) << ".\n";
+            return RetCodes::ERR_CONNECTION_FAILED;
+        }
+        player[i] = client_fd;
+        std::cout << "Player " << (i + 1) << " connected.\n";
+    }
+
+    return RetCodes::SUCCESS;
+}
