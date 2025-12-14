1ab3587098b61b7057dd0e4f20338759ab4d733a
Roman Maltsev <roman.maltsev.realgar@proton.me>
2025-12-14 12:35:52 +0300

    style: little changes

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -9,24 +9,28 @@
 RetCodes server(Rules game_rules = Rules()) {
     size_t turn = 0;
     if (game_rules.players != 2) {
-        perror("Error: Only 2 players are supported in this version.\n");
+        std::cerr << "Error: Only 2 players are supported in this version.\n";
         return RetCodes::ERR_NOT_SUPPORTED;
     }
-    int server = socket(AF_INET, SOCK_STREAM, 0);
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd < 0) {
+        perror("socket() failed");
+        return RetCodes::ERR_INTERNAL;
+    }
     sockaddr_in addr{};
     addr.sin_family = AF_INET;
     addr.sin_port = htons(8080);
     addr.sin_addr.s_addr = INADDR_ANY;
 
-    if (bind(server, (sockaddr *)&addr, sizeof(addr)) == -1) {
+    if (bind(server_fd, (sockaddr *)&addr, sizeof(addr)) == -1) {
         perror("bind() failed");
         return RetCodes::ERR_INTERNAL;
     }
     int player[2];
+    listen(server_fd, SOMAXCONN);
     for (size_t i = 0; i < 2; ++i) {
         std::cout << "Waiting for player " << (i + 1) << " to connect...\n";
-        listen(player[i], SOMAXCONN);
-        int client_fd = accept(player[i], nullptr, nullptr);
+        int client_fd = accept(server_fd, nullptr, nullptr);
         if (client_fd < 0) {
             std::cerr << "Error: Failed to accept connection for player " << (i + 1) << ".\n";
             return RetCodes::ERR_CONNECTION_FAILED;
